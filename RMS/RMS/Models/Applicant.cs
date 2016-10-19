3eee736beb1757471650b220a0a932c36596d495
Moinul Islam <moinulmithu@hotmail.com>
2016-10-19 11:03:52 +0600

    applicant added

---
+++ RMS/RMS/Models/Applicant.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RMS.Models
+{
+    public class Applicant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
